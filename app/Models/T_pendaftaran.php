5acb2a668cf80d075f2968e788eaa03f8a7a7fe5
asrani <asrani.27@gmail.com>
2023-06-07 08:00:17 +0800

    f

--- app/Models/T_pendaftaran.php
+++ app/Models/T_pendaftaran.php
@@ -10,4 +10,28 @@ class T_pendaftaran extends Model
     use HasFactory;
     protected $table = 't_pendaftaran';
     protected $guarded = ['id'];
+    public function anamnesa()
+    {
+        return $this->hasOne(T_anamnesa::class, 't_pendaftaran_id');
+    }
+
+    public function pasien()
+    {
+        return $this->belongsTo(M_pasien::class, 'm_pasien_id');
+    }
+
+    public function diagnosa()
+    {
+        return $this->hasMany(T_diagnosa::class, 't_pendaftaran_id');
+    }
+
+    public function resep()
+    {
+        return $this->hasMany(T_resep::class, 't_pendaftaran_id');
+    }
+
+    public function tindakan()
+    {
+        return $this->hasMany(T_tindakan::class, 't_pendaftaran_id');
+    }
 }
